f02d0d9e0ec8cafdde533eb0752cb714801b1e13
kalema1 <kalemar8@gmail.com>
2024-03-12 16:20:24 +0300

    setup pages for the project
    - created pages for the project.
    - added routes to the created pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <div>Hello vite</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
