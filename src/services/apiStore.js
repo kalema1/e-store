cbf396d9fce98abd9d8fc3108882c0aabf2c349f
kalema1 <kalemar8@gmail.com>
2024-03-25 11:50:36 +0300

    feat: fetch data from API
    - created a function data fectches data from API.
    - function returns fetched data.

--- src/services/apiStore.js
+++ src/services/apiStore.js
@@ -1,1 +1,15 @@
 const API_URL = "https://fakestoreapi.com/products";
+
+/**
+ * gets product from API
+ */
+export async function getProducts() {
+  const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("Failed getting products");
+  }
+
+  const data = await response.json();
+  return data;
+}
