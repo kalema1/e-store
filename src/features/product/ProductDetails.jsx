376526a25c89941b188ed5de7c19102378978277
kalema1 <kalemar8@gmail.com>
2024-04-08 15:24:14 +0300

    styles: add styles for the ProductDetails component
    - added styles for the image.
    - added styles for the text.
    - added styles for the emoji rating.
    - added styles for the buttons.

--- src/features/product/ProductDetails.jsx
+++ src/features/product/ProductDetails.jsx
@@ -1,36 +1,46 @@
 import { Link } from "react-router-dom";
 import Button from "../../ui/Button";
 import styles from "./ProductDetails.module.css";
+import Header from "../Header";
 
 export default function ProductDetails() {
   return (
-    <section className={styles.productDetailsSection}>
-      <div className={styles.productDetailsContainer}>
-        <div className={styles.grid}>
-          <div></div>
-          <div className={styles.productdetails}>
-            <p className={styles.category}>MENS CLOTHING</p>
-            <h2 className={styles.title}>
-              Fjallraven - Foldsack No.1 Backpack, Fits 15 laptops
-            </h2>
-            <p className={styles.rating}>
-              rating 3.9 <span className={styles.star}>⭐</span>
-            </p>
-            <p className={styles.description}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              soluta sequi sint natus similique laborum rem obcaecati debitis in
-              dolorum impedit ratione expedita autem cumque, fuga amet veniam!
-              Dolor, impedit.
-            </p>
+    <>
+      <Header />
+      <section className={styles.productDetailsSection}>
+        <div className="container">
+          <div className={styles.grid}>
             <div>
-              <Button>Add to Cart</Button>
-              <Link to="/cart">
-                <Button>Go to Cart</Button>
-              </Link>
+              <img
+                className={styles.img}
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                alt="mens"
+              />
+            </div>
+            <div className={styles.productdetails}>
+              <p className={styles.category}>MENS CLOTHING</p>
+              <h2 className={styles.title}>
+                Fjallraven - Foldsack No.1 Backpack, Fits 15 laptops
+              </h2>
+              <p className={styles.rating}>
+                Rating 3.9 <span className={styles.star}>⭐</span>
+              </p>
+              <p className={styles.price}>$ 109.05</p>
+              <p className={styles.description}>
+                Your perfect pack for everyday use and walks in the forest.
+                Stash your laptop (up to 15 inches) in the padded sleeve, your
+                everyday
+              </p>
+              <div className={styles.btnContainer}>
+                <Button>Add to Cart</Button>
+                <Link className={styles.link} to="/cart">
+                  <Button>Go to Cart</Button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
