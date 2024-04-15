5a33988a3671dd8f65d61a3970bf81cd41dc4402
kalema1 <kalemar8@gmail.com>
2024-04-15 20:31:51 +0300

    feat: create cartSlice for the cart
    - created the cartSlice for the cart using createSlice from redux
      toolkit.
    - added the name cart to the cartSlice.
    - added initialState to the cartSlice.
    - created a fake initialState for development purpose.
    - added action reducers to the cartslice.

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,11 +1,71 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cart: [],
+  //cart: [],
+  cart: [
+    {
+      id: 1,
+      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      price: 109.95,
+      quantity: 2,
+      totalPrice: 219.9,
+    },
+
+    {
+      id: 2,
+      title: "Mens Casual Premium Slim Fit T-Shirts",
+      price: 22.3,
+      quantity: 3,
+      totalPrice: 66.9,
+    },
+  ],
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addItem(state, action) {
+      // payload = new item
+      state.cart.push(action.payload);
+    },
+    deleteitem(state, action) {
+      // payload = product ID
+      // find the product with that ID and then delete it
+      state.cart = state.cart.filter(
+        (product) => product.id !== action.payload
+      );
+    },
+    increeaseItemQuantity(state, action) {
+      // find that product and then mutate the quantity on that product
+      const product = state.cart.find(
+        (product) => product.id === action.payload
+      );
+
+      product.quantity++;
+      product.totalPrice = product.price * product.quantity;
+    },
+    decreaseItemQuantity(state, action) {
+      // find that product and then mutate the quantity on that product
+      const product = state.cart.find(
+        (product) => product.id === action.payload
+      );
+
+      product.quantity--;
+      product.totalPrice = product.price * product.quantity;
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
 });
+
+export const {
+  addItem,
+  increeaseItemQuantity,
+  decreaseItemQuantity,
+  deleteitem,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
