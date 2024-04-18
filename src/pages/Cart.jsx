1ce6635d47f87849ec480edecf26a53ce6aef6a0
kalema1 <kalemar8@gmail.com>
2024-04-18 11:27:43 +0300

    add Header component in the Cart component
    - added the Header component into the Cart component.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,27 @@
 import { useSelector } from "react-redux";
+
 import styles from "./Cart.module.css";
 import { getCart } from "../features/cart/cartSlice";
 import CartItem from "../features/cart/CartItem";
+import Header from "../features/Header";
 
 export default function Cart() {
   const cart = useSelector(getCart);
 
   return (
-    <section className={styles.cartSection}>
-      <div className="container">
-        <div>
-          <ul>
-            {cart.map((product) => (
-              <CartItem product={product} key={product.id} />
-            ))}
-          </ul>
+    <>
+      <Header />
+      <section className={styles.cartSection}>
+        <div className="container">
+          <div>
+            <ul>
+              {cart.map((product) => (
+                <CartItem product={product} key={product.id} />
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
