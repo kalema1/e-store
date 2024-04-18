232c22845536c9551a292695aca51724a075db3b
kalema1 <kalemar8@gmail.com>
2024-04-18 14:09:11 +0300

    add image in the EmptyCart component
    - added image element in the EmptyCart component.
    - added styles for the image element.

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -7,11 +7,14 @@ export default function EmptyCart() {
     <section className={styles.emptyCartSection}>
       <div className="container">
         <div className={styles.emptyCartContainer}>
+          <div className={styles.imgContainer}>
+            <img className={styles.img} src="cart1.jpg" alt="cart" />
+          </div>
           <h2 className={styles.heading}>Your Cart is Empty</h2>
           <p className={styles.paragraph}>
-            Browse Our categories and discover the best deals!
+            Browse our categories and discover the best deals!
           </p>
-          <Link className={styles.link} to="/product">
+          <Link className={styles.link} to="/products">
             <Button>START SHOPPING</Button>
           </Link>
         </div>
